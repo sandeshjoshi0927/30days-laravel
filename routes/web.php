3e893f57fc2a12955ecc0dc66e96ae3a5b1ddc1d
sandeshjoshi0927 <sandeshjoshi2211@gmail.com>
2025-03-04 17:24:08 +0545

    Jobs class and fns

--- routes/web.php
+++ routes/web.php
@@ -1,58 +1,22 @@
 <?php
 
-use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
+use App\Models\Job;
 
 Route::get('/', function () {
     return view(view: 'home');
 });
 
 Route::get('/jobs', function () {
-    $jobs = [
-        [
-            'id' => 1,
-            'title' => 'Teacher',
-            'salary' => '$20,000'
-        ],
-        [
-            'id' => 2,
-            'title' => 'Player',
-            'salary' => '$30,000'
-        ],
-        [
-            'id' => 3,
-            'title' => 'Programmer',
-            'salary' => '$60,000'
-        ]
-    ];
+
     return view('jobs', [
-        'jobs' => $jobs
+        'jobs' => Job::all()
     ]);
 });
 
 Route::get('/jobs/{id}', function ($id) {
-    $jobs = [
-        [
-            'id' => 1,
-            'title' => 'Teacher',
-            'salary' => '$20,000'
-        ],
-        [
-            'id' => 2,
-            'title' => 'Player',
-            'salary' => '$30,000'
-        ],
-        [
-            'id' => 3,
-            'title' => 'Programmer',
-            'salary' => '$60,000'
-        ]
-    ];
-
-    $job = Arr::first($jobs, fn($job) => $job['id'] == $id);
-
     return view('job', [
-        'job' => $job
+        'job' => Job::find($id)
     ]);
 });
 
